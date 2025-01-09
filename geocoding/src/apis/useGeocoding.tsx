45fc433474e18340bdeeac4dc02705bf336b909b
garam.dev <garam-park@naver.com>
2025-01-09 16:15:41 +0900

    geocoding 처리

--- geocoding/src/apis/useGeocoding.tsx
+++ geocoding/src/apis/useGeocoding.tsx
@@ -1,31 +1,73 @@
 import React from "react";
-import axios from "axios";
-import jsonAdapter from "axios-jsonp";
+import $ from "jquery";
 
 const apikey = import.meta.env.VITE_APP_NGII_API_KEY;
 
+interface GeocodingResponse {
+  search: Search;
+}
+
+interface Search {
+  header: Header;
+  contents: Content;
+}
+
+interface Content {
+  geo: Geo;
+}
+
+interface Geo {
+  adresTy: string;
+  adres: string;
+  bdMgtSn: string;
+  pnu: string;
+  x: string;
+  y: string;
+}
+
+interface Header {
+  responseCode: number;
+  responseMessage: string;
+  target: string;
+  juso: string;
+  totalCount: number;
+}
+
 export default function useGeocoding() {
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [content, setContent] = React.useState<Content | null>(null);
+  const [error, setError] = React.useState<string | null>(null);
+
   const fetchResult = React.useCallback(async (juso: string) => {
-    const target = "geo";
-    // const target = "des";
+    setLoading(true);
+    $.ajax({
+      type: "GET",
+      url: "http://map.ngii.go.kr/openapi/search.json",
+      data: {
+        target: "geo",
+        apikey,
+        juso,
+      },
+      dataType: "jsonp",
+      crossDomain: true,
+      success: function (result: GeocodingResponse) {
+        if (result.search.header.responseCode !== 0) {
+          console.log(result.search);
+          setError(result.search.header.responseMessage);
+        } else {
+          console.log(result.search);
 
-    const refrnUrl = "http://localhost:5175";
-
-    const apiUrl = "/openapi/search.json";
-    const params = {
-      target: target,
-      apikey: apikey,
-      refrnUrl: refrnUrl,
-      juso: encodeURIComponent(juso),
-      // juso,
-    };
-
-    return axios.get(apiUrl, {
-      params: { ...params, callbackParamName: "callback" },
-      adapter: jsonAdapter,
-      // headers: { Accept: "application/json" },
+          setContent(result.search.contents);
+        }
+      },
+      error: function (xhr, ajaxSettings, thrownError) {
+        setError(thrownError);
+      },
+      complete: function () {
+        setLoading(false);
+      },
     });
   }, []);
 
-  return { fetchResult };
+  return { fetchResult, content, error, loading };
 }
